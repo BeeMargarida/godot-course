cf69b36b72ce0643228e8b549d5d032bd995b9f5
BeeMargarida <up201505505@gcloud.fe.up.pt>
2019-07-20 22:40:44 +0100

    CameraBody field of view working

--- Heist_Meisters/Scenes/Characters/CameraBody.gd
+++ Heist_Meisters/Scenes/Characters/CameraBody.gd
@@ -1,12 +1,21 @@
 extends "res://Scripts/Character.gd"
 
-const FOV_TOLERANCE : int = 45
+const FOV_TOLERANCE : int = 22
+const RED: Color = Color(1,.25,.25)
+const WHITE : Color = Color(1,1,1)
 
 onready var Player : Node = get_node("/root/Level1/Player") # Make this level neutral
 
 func _process(delta: float) -> void:
-	Player_is_in_FOV_TOLERANCE()
+	if Player_is_in_FOV_TOLERANCE():
+		$Torch.color = RED
+	else:
+		$Torch.color = WHITE
 	
-func Player_is_in_FOV_TOLERANCE() -> void:
-	var NPC_facing_direction : Vector2 = Vector2(1,0).rotated(global_rotation)
+func Player_is_in_FOV_TOLERANCE() -> bool:
+	var NPC_facing_direction : Vector2 = Vector2(1,0).rotated(global_rotation) # direction the NPC is facing
+	var direction_to_Player : Vector2 = (Player.position - global_position).normalized()
 	
+	if abs(direction_to_Player.angle_to(NPC_facing_direction)) < deg2rad(FOV_TOLERANCE):
+		return true
+	return false
